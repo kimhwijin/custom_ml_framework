b10c6c6cd50ea043c6f567fa3f678b5c4592687b
kimhwijin <rlagnlwls0160@gmail.com>
2022-03-08 15:43:29 +0900

    load csv dataset cpp, h define

--- c++/init_model.cpp
+++ c++/init_model.cpp
@@ -1,4 +1,14 @@
 //
 // Created by 김휘진 on 2022/03/08.
 //
+#include "LoadCSVDataset.h"
+#include <map>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+int main(){
+    std::map<int, std::vector<string>> dataset = loadCSVDataset();
+    std::cout << dataset[0][0];
+}
