b6e751b277c425fdacb8c939facdc259873671c1
kimhwijin <rlagnlwls0160@gmail.com>
2022-03-13 02:06:11 +0900

    selu, softplut, softsign, sigmooid, hard sigmoid, tanh, hard tanh, exponential

--- python/activation/activations.py
+++ python/activation/activations.py
@@ -7,22 +7,58 @@ def softmax(x, axis=-1):
         output = e / s
     else:
         raise ValueError()
-    return {'output': output, 'logits': x}
+    return output, x
 
 def elu(x, alpha=1.0):
     output = np.where(x > 0, x, alpha * (np.exp(x) - 1))
-    return {'output': output}
+    return output
+
+def selu(x):
+    alpha = 1.67326324
+    scale = 1.05070098
+    return scale * elu(x, alpha)
 
 def relu(x):
     ouptut = np.maximum(x, 0)
-    return {'output': ouptut}
+    return ouptut
 
+def softplus(x):
+    output = np.log(np.exp(x) + 1)
+    return output, x
+
+def softsign(x):
+    output = x / (np.abs(x) + 1)
+    return output, x
+
+def leaky_relu(x, alpha=0.3):
+    output = np.where(x > 0, x, alpha * x)
+    return output
+
+def sigmoid(x):
+    output = 1 / (1 + np.exp(-x))
+    return output
+
+def hard_sigmoid(x):
+    # x < -2.5 : 0
+    # -2.5 <= x <= 2.5 : 0.2 * x + 0.5
+    # 2.5 < x : 1
+    output = np.where(x > 2.5, 1, np.where(x < -2.5, 0, 0.2 * x + 0.5))
+    return output
+
+def tanh(x):
+    output = 2 * sigmoid(2*x) - 1
+    return output
+
+def hard_tanh(x):
+    output = 2 * hard_sigmoid(2 * x) - 1
+    return output
+
+def exponential(x):
+    output = np.exp(x)
+    return output
 
 def relu_derv(y):
     return np.sign(y)
-
-def sigmoid(x):
-    return np.exp(-relu(x)) / (1.0 + np.exp(-np.abs(x)))
 
 def sigmoid_cross_entropy_with_logits(z, x):
     return relu(x) - x * z + np.log(1 + np.exp(-np.abs(x)))
