fe86b05b4af6890b21ac6ba26fc1b78335bf2d1f
kimhwijin <rlagnlwls0160@gmail.com>
2022-04-03 20:22:06 +0900

    Monte carlo Dropout 구현

--- python/layer/dropout.py
+++ python/layer/dropout.py
@@ -5,18 +5,18 @@ from base_layer import Layer
 class Dropout(Layer):
     def __init__(self,
                 rate,
-                training=True,
                 seed=None,
+                training=True,
                 dtype=np.float32
                 ):
 
         self.rate = rate
         self.seed = seed
         self.training = training
+        self.dtype = dtype
 
     def forward(self, inputs):
         x = inputs
-
         if not self.training:
             return x
         
@@ -30,5 +30,33 @@ class Dropout(Layer):
         return dLdx
 
         
+class MCDropout(Layer):
+    def __init__(self,
+                rate,
+                n_iters,
+                seed=None,
+                training=True,
+                dtype=np.float32):
 
+        self.dropout = Dropout(rate, seed, training, dtype)
+        self.n_iters = n_iters
+    
+    def forward(self, inputs):
+        x = inputs
 
+        if not self.training:
+            x = np.expand_dims(x, axis=0)
+            y = self.dropout(x)
+            for _ in range(self.n_iters-1):
+                y = np.r_[y, self.dropout(x)]
+            return y
+
+        y = self.dropout(x)
+
+        return y
+            
+    def backprop(self, dLdy):
+        dLdx = self.dropout.backprop(dLdy)
+        return dLdx
+        
+    
